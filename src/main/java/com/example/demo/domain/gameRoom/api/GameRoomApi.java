90464afd3b26d0a67b31e291ae6bf7474be9497d
weightsforfun <giyoung914@gmail.com>
2023-05-18 13:12:33 +0900

    Feat: gameRoom api 생성
    gameRoom api 생성

--- src/main/java/com/example/demo/domain/gameRoom/api/GameRoomApi.java
+++ src/main/java/com/example/demo/domain/gameRoom/api/GameRoomApi.java
@@ -7,10 +7,13 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("/game-rooms")
 public class GameRoomApi {
+    private final SimpMessagingTemplate template;
 
     @GetMapping()
     public List<GameRoom> findAllGameRooms(){
@@ -40,7 +44,20 @@ public class GameRoomApi {
                 .build();
         return gameRoom.getId()+"is created";
     }
-
+    @GetMapping("/{id}")
+    public String findGameRoom(@PathVariable("id") Long id){
+        //game room 가져와서 인원수 체크
+        //인원수가 4명미만이면 success
+        //아니면 fail
+        //interceptor로 동시에 접근할때도 막아준다.
+        int count=3;
+        if(count<4){
+            return "OK" + id;
+        }
+        else{
+            return "FULL";
+        }
+    }
 
 
 
