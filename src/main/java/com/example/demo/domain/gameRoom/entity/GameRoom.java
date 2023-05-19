228837859f162b7fad9ffdb988de535c318de00f
weightsforfun <giyoung914@gmail.com>
2023-05-19 12:28:08 +0900

    Fix: GameRoom Entity 어노테이션 추가
    NoArgsConstructor, AllArgsConstructor 추가

--- src/main/java/com/example/demo/domain/gameRoom/entity/GameRoom.java
+++ src/main/java/com/example/demo/domain/gameRoom/entity/GameRoom.java
@@ -4,12 +4,16 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GameRoom {
     @Id
     @GeneratedValue
