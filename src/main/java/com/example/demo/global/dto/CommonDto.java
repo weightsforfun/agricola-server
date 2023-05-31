11db693a29148af2a5ea5840e0ff0fdb8b9d3891
weightsforfun <giyoung914@gmail.com>
2023-05-31 11:07:37 +0900

    Fix: CommonReq, CommonRes -> CommonDto 로 통합
    CommonReq, CommonRes -> CommonDto 로 통합

--- src/main/java/com/example/demo/global/dto/CommonDto.java
+++ src/main/java/com/example/demo/global/dto/CommonDto.java
@@ -15,11 +15,12 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommonDto {
+    private MessageType messageType;
     private Long roomId;
     private int round; //현재 라운드
-    private List<Integer> action = new ArrayList<>(); //행동버튼 눌림 여부
+    private List<List<Integer>> action = new ArrayList<>(); //행동버튼 눌림 여부
 
     private int currentTurn ; // 변경된 순서
-    private List<List<Integer>> turnArray; //[[turn,남은농부수],[turn,남은농부수],[turn,남은농부수]]
+    private List<Integer> farmer_count; //농부 수
 
 }
