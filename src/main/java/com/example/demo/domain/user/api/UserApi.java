10d6cf7ced9f3d88ac6c5f4bbfaaa50df7d3002c
weightsforfun <giyoung914@gmail.com>
2023-05-31 11:24:35 +0900

    Fix: commonDto 통합에 따른 userApi 수정
    commonDto 통합에 따른 userApi 수정

--- src/main/java/com/example/demo/domain/user/api/UserApi.java
+++ src/main/java/com/example/demo/domain/user/api/UserApi.java
@@ -1,20 +1,15 @@
 package com.example.demo.domain.user.api;
 
-import com.example.demo.domain.gameRoom.entity.GameRoom;
-import com.example.demo.domain.gameRoom.repository.GameRoomRepository;
 import com.example.demo.domain.user.service.UserService;
 import com.example.demo.global.config.websocketConfig.StompPrincipal;
 import com.example.demo.global.dto.CommonDto;
-import com.example.demo.global.dto.CommonReq;
-import java.lang.reflect.Array;
-import java.security.Principal;
+import com.example.demo.global.dto.MessageType;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
@@ -27,47 +22,48 @@ public class UserApi {
 
 
     @MessageMapping(value = "/user/init")
-    public void intiUser(StompPrincipal principal,CommonReq request){
-        Integer turn = userService.initUser(request.getRoomId());
+    public void intiUser(StompPrincipal principal,CommonDto req){
+        Integer turn = userService.initUser(req.getRoomId());
         if(turn>0 && turn<4) {
-            log.info(request.getRoomId().toString());
+            log.info(req.getRoomId().toString());
             template.convertAndSendToUser(principal.getName(),
-                    "/sub/game-room/"+request.getRoomId()
+                    "/sub/game-room/"+req.getRoomId()
                     , "OK"+" "+turn);
-            log.info("working");
+
 
 
         }
         else if(turn==4){
-            List<List<Integer>> turnArray= new ArrayList<>();
-            for(int i=0;i<4;i++){
-                turnArray.add(new ArrayList<>(Arrays.asList(i,2)));
+            List<List<Integer>> turnArray= new ArrayList<>();//행동버튼 초기화
+            for(int i=0;i<13;i++){
+                turnArray.add(new ArrayList<>(Arrays.asList(0,3)));
             }
-            CommonDto commonDto = CommonDto.builder()
+            CommonDto res = CommonDto.builder()
+                    .messageType(MessageType.INIT)
                     .round(1)
                     .currentTurn(1)
-                    .roomId(request.getRoomId())
-                    .action(new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)))
-                    .turnArray(turnArray)
-
+                    .roomId(req.getRoomId())
+                    .action(turnArray)
+                    .farmer_count(new ArrayList<>(Arrays.asList(2,2,2,2)))
                     .build();
-            log.info(request.getRoomId().toString());
-            log.info(request.getContent().toString());
-            template.convertAndSend("/sub/game-room/"+request.getRoomId()
-                    ,commonDto);
-            template.convertAndSendToUser(principal.getName(),"/sub/game-room/"+request.getRoomId(),"OK"+" "+turn);
+
+
+
+            template.convertAndSendToUser(principal.getName(),"/sub/game-room/"+req.getRoomId(),"OK"+" "+turn);
+            template.convertAndSend("/sub/game-room/"+req.getRoomId()
+                    , res);
             log.info("send turn");
 
         }
         else{
             template.convertAndSendToUser(principal.getName(),
-                    "/sub/game-room/" + request.getRoomId()
+                    "/sub/game-room/" + req.getRoomId()
                     , "FULL");
         }
 
     }
     @MessageMapping("/user/test")
-    public void test(CommonReq request){
-        template.convertAndSend("/sub/game-room/"+request.getRoomId(),"hello");
+    public void test(CommonDto commonDto){
+        template.convertAndSend("/sub/game-room/"+commonDto.getRoomId(),"hello");
     }
 }
