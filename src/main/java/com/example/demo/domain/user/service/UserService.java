34c5c0891f4a9ba720eadbe77e326d272884508f
weightsforfun <giyoung914@gmail.com>
2023-06-01 13:43:38 +0900

    Feat: userService card 배정 로직 추가
    userService card 배정 로직 추가
    user 입장시 직업,보조 카드 랜덤으로 유저한테 배정

--- src/main/java/com/example/demo/domain/user/service/UserService.java
+++ src/main/java/com/example/demo/domain/user/service/UserService.java
@@ -2,7 +2,11 @@ package com.example.demo.domain.user.service;
 
 import com.example.demo.domain.gameRoom.entity.GameRoom;
 import com.example.demo.domain.gameRoom.repository.GameRoomRepository;
+import com.example.demo.domain.user.InitUserDto;
 import com.example.demo.domain.user.repository.UserRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import javax.persistence.LockModeType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.Lock;
@@ -17,21 +21,63 @@ public class UserService {
     private final UserRepository userRepository;
     @Transactional
     @Lock(value = LockModeType.PESSIMISTIC_READ)
-    public Integer initUser(Long gameRoomId){
+    public InitUserDto initUser(Long gameRoomId){
         GameRoom gameRoom = gameRoomRepository.findById(gameRoomId)
                 .orElseThrow(() -> new RuntimeException("no game room"));
+
+
+
         if(gameRoom.getPeopleCount()<4){
+            List<Integer> jobCards = gameRoom.getJobCards();
+            List<Integer> subCards = gameRoom.getSubCards();
+
+            List<Integer> randomJobCards = makeRandomCardList(jobCards);
+            List<Integer> randomSubCards = makeRandomCardList(subCards);
+
+
+
             gameRoom.addUserCount();
-            return gameRoom.getPeopleCount();
+            InitUserDto initUserDto = InitUserDto.builder()
+                    .enter(true)
+                    .jobCards(randomJobCards)
+                    .subCards(randomSubCards)
+                    .turn(gameRoom.getPeople())
+                    .build();
+
+            return initUserDto;
 
         }
         else {
-            return -1;
+            InitUserDto initUserDto = InitUserDto.builder()
+                    .enter(false)
+                    .build();
+            return initUserDto;
         }
     }
-    public void initGame(){
-        //user card 분배
-        //행동 버튼 상태,round, turn api 계층에 전달
+
+    private static List<Integer> makeRandomCardList(List<Integer> numbers){
+        List<Integer> randomNumbers = getRandomNumbers(numbers, 7);
+
+        return randomNumbers;
     }
+    private static List<Integer> getRandomNumbers(List<Integer> numbers, int count) {
+        List<Integer> randomNumbers = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < count; i++) {
+            int randomIndex = random.nextInt(numbers.size());
+            int randomNumber = numbers.get(randomIndex);
+
+            randomNumbers.add(randomNumber);
+
+            numbers.remove(randomIndex);
+
+        }
+
+        return randomNumbers;
+    }
+
+
+
 
 }
